ad6a3a5f349345140b8463b78e36792dc35aa8fc
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2023-06-03 21:56:15 +0900

    created program

---
+++ Main.kt
@@ -0,0 +1,17 @@
+import java.lang.NumberFormatException
+
+fun main() {
+    println("please enter kilometers digit until decimal separator you want to convert to mile.")
+    val kilo = readLine()
+    if (kilo.isNullOrEmpty()) {
+        println("please press enter key AFTER ENTER DIGIT.")
+    }
+    try {
+        val inputKilo = kilo!!.toDouble()
+        val kiloMile = kiloMile()
+        println(kiloMile.convertKtoM(inputKilo))
+    } catch (e: NumberFormatException) {
+        println("would you enter digit?")
+    }
+
+}
---
+++ kiloMile.kt
@@ -0,0 +1,6 @@
+open class kiloMile {
+    fun convertKtoM(kilo:Double) {
+        val mile = kilo / 1.60934
+        println("converted kilometers to mile: $mile mp")
+    }
+}
