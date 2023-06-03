b2dae45ce593538832e1d9979a17ad9f6644ea70
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2023-06-03 21:57:30 +0900

    formated lines

--- kiloMile.kt
+++ kiloMile.kt
@@ -1,5 +1,5 @@
 open class kiloMile {
-    fun convertKtoM(kilo:Double) {
+    fun convertKtoM(kilo: Double) {
         val mile = kilo / 1.60934
         println("converted kilometers to mile: $mile mp")
     }
