b1e33fe8d9995f05c7ba62b4190546a936efed97
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2023-06-05 21:18:59 +0900

    added mile to kilo

--- kiloMile.kt
+++ kiloMile.kt
@@ -3,4 +3,9 @@ open class kiloMile {
         val mile = kilo / 1.60934
         println("converted kilometers to mile: $mile mp")
     }
+
+    fun convertMtoK(mile: Double) {
+        val kilo = mile * 1.60934
+        println("converted mile points to kilometers: $kilo km")
+    }
 }
